44e0d8483bb3cfbd5facab2faf81f532061d32e9
gallonp <gallonpig@gmail.com>
2014-02-08 22:16:05 -0500

    first commit

---
+++ src/com/example/giambiserver/RegisterAndLoginServerlet.java
@@ -0,0 +1,66 @@
+package com.example.giambiserver;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import com.google.appengine.api.datastore.Entity;
+
+@SuppressWarnings("serial")
+public class RegisterAndLoginServerlet extends HttpServlet {
+	private static final Logger logger = Logger
+			.getLogger(RegisterAndLoginServerlet.class.getCanonicalName());
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		Entity user = UserAccount.getSingleUser("gallonpig");
+		String usr = (String) user.getProperty("username");
+		String pwd = (String) user.getProperty("password");
+		resp.setContentType("text/plain");
+		resp.getWriter().println(usr + " " + pwd);
+
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		// Need parse safty check!!!!!
+		String data = req.getParameter("json");
+		String decodedContent = "";
+		if (data != null) {
+			decodedContent = URLDecoder.decode(data, "UTF-8");
+		}
+		// String content = Util.getBody(req);
+		JSONObject job = (JSONObject) JSONValue.parse(decodedContent);
+		//test
+		resp.getWriter().print(decodedContent);
+
+		String set = job.toJSONString();
+		String password = (String) job.get("password");
+		String username = (String) job.get("username");
+		//
+		// need authenticate
+		resp.setContentType("text/plain");
+		PrintWriter out = resp.getWriter();
+		Boolean createSuccess = false;
+		if (!password.isEmpty() && !username.isEmpty()) {
+			createSuccess = UserAccount.createUserAccount(username, password);
+		} else {
+			out.print("Invalid request: no password or username");
+		}
+		if (createSuccess) {
+			out.print("Successfully created user: " + username);
+		} else {
+			out.print("Failed to create user. ");
+		}
+	}
+
+}
---
+++ src/com/example/giambiserver/UserAccount.java
@@ -0,0 +1,57 @@
+package com.example.giambiserver;
+
+import java.util.List;
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+
+public class UserAccount {
+
+	public static boolean createUserAccount(String username, String password) {
+		Entity userAccount = getSingleUser(username);
+		if (userAccount == null) {
+			userAccount = new Entity("UserAccount");
+			userAccount.setProperty("username", username);
+			userAccount.setProperty("password", password);
+		} else {
+			return false;
+		}
+		Util.persistEntity(userAccount);
+		return true;
+	}
+
+	public static boolean updateUserAccount(String username, String password) {
+		Entity userAccount = getSingleUser(username);
+		if (userAccount == null) {
+			return false;
+		} else {
+			userAccount.setProperty("password", password);
+		}
+		Util.persistEntity(userAccount);
+		return false;
+	}
+
+	/**
+	 * get Item with item name
+	 * 
+	 * @param itemName
+	 *            : get itemName
+	 * @return item entity
+	 */
+	@SuppressWarnings("deprecation")
+	public static Entity getSingleUser(String username) {
+		Query query = new Query("UserAccount");
+		Filter singleUserFilter = new Query.FilterPredicate("username", FilterOperator.EQUAL, username);
+		query.setFilter(singleUserFilter);
+		List<Entity> results = Util.getDatastoreServiceInstance()
+				.prepare(query).asList(FetchOptions.Builder.withDefaults());
+		if (!results.isEmpty()) {
+			return (Entity) results.remove(0);
+		}
+		return null;
+	}
+
+}
