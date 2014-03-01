cb52478600de6bb9a16cb7d18fecf78005f47bc5
wchen342 <feiyu2817@live.cn>
2014-03-02 05:01:48 +0900

    Login response text changed

--- src/com/example/giambiserver/LoginServlet.java
+++ src/com/example/giambiserver/LoginServlet.java
@@ -18,43 +18,43 @@ import org.json.simple.JSONValue;
 @SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet {
 
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
 
-	}
+    }
 
-	public void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		
-		String data = req.getParameter("json");
-		String decodedContent = "";
-		if (data != null) {
-			decodedContent = URLDecoder.decode(data, "UTF-8");
-		} else {
-			throw new IOException("Data illegal.");
-		}
-		JSONObject job = (JSONObject) JSONValue.parse(decodedContent);
-		resp.getWriter().print(decodedContent);
-		String password = (String) job.get("password");
-		String username = (String) job.get("username");
-		
-		switch (UserAccount.authentication(username, password)) {
-			case 1:
-				resp.getWriter().println("Login succeeded!");
-				Calendar cal = Calendar.getInstance();
-				String userAndTime = username +","+ cal.getTime().toString();
-				Cookie cookie = new Cookie("auth-cookie", userAndTime);
-				cookie.setMaxAge(120);
-				SessionCookie.createSessionCookie(username, cookie);
-				resp.addCookie(cookie);
-				break;
-			case 0:
-				resp.getWriter().println("Password doesn't match username.");
-				break;
-			default:
-				resp.getWriter().println("Username hasn't been registered.");	
-		}
-		/*	Entity user = UserAccount.getSingleUser(username);
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+
+        String data = req.getParameter("json");
+        String decodedContent = "";
+        if (data != null) {
+            decodedContent = URLDecoder.decode(data, "UTF-8");
+        } else {
+            throw new IOException("Data illegal.");
+        }
+        JSONObject job = (JSONObject) JSONValue.parse(decodedContent);
+//        resp.getWriter().print(decodedContent);
+        String password = (String) job.get("password");
+        String username = (String) job.get("username");
+
+        switch (UserAccount.authentication(username, password)) {
+            case 1:
+                resp.getWriter().println("Login succeeded!");
+                Calendar cal = Calendar.getInstance();
+                String userAndTime = username + "," + cal.getTime().toString();
+                Cookie cookie = new Cookie("auth-cookie", userAndTime);
+                cookie.setMaxAge(120);
+                SessionCookie.createSessionCookie(username, cookie);
+                resp.addCookie(cookie);
+                break;
+            case 0:
+                resp.getWriter().println("Password doesn't match username.");
+                break;
+            default:
+                resp.getWriter().println("Username hasn't been registered.");
+        }
+        /*	Entity user = UserAccount.getSingleUser(username);
 			if (user!=null){
 			String dbUsername = (String) user.getProperty("username");
 			String dbPassword = (String) user.getProperty("password");
@@ -72,5 +72,5 @@ public class LoginServlet extends HttpServlet {
 		} else {
 			resp.getWriter().println("Username hasn't been registered.");
 		}*/
-	}
+    }
 }
