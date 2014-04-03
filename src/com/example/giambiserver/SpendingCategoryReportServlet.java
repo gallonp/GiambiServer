427215e267b3bbdf4b4c7b6c9048bc1570f328cb
Leonard Hsin <leonardhsin@gmail.com>
2014-04-03 02:05:38 -0400

    Spending category report support date; minor bug fix.

--- src/com/example/giambiserver/SpendingCategoryReportServlet.java
+++ src/com/example/giambiserver/SpendingCategoryReportServlet.java
@@ -12,9 +12,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URLDecoder;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.logging.Level;
+import java.util.Date;
 
 public class SpendingCategoryReportServlet extends HttpServlet {
     //private enum FIELDS {Category, Amount}
@@ -26,25 +29,55 @@ public class SpendingCategoryReportServlet extends HttpServlet {
             throws IOException {
         String data = req.getParameter("json");
         String decodedContent;
+        PrintWriter respWriter = resp.getWriter();
         if (data != null) {
             decodedContent = URLDecoder.decode(data, "UTF-8");
         } else {
+        	respWriter.print("Data is null.");
             throw new IOException("Data is null");
         }
         JSONObject job = (JSONObject) JSONValue.parse(decodedContent);
-        PrintWriter respWriter = resp.getWriter();
         String userAccount = (String) job.get("userAccount");
+        String startDate = (String) job.get("startDate");
+        Date dStartDate = new Date(Integer.valueOf(startDate.substring(6)) - 1900,
+        		Integer.valueOf(startDate.substring(0, 2)) - 1,
+        			Integer.valueOf(startDate.substring(3, 5)));
+        String endDate = (String) job.get("endDate");
+        Date dEndDate = new Date(Integer.valueOf(endDate.substring(6)) - 1900,
+        		Integer.valueOf(endDate.substring(0, 2)) - 1,
+        			Integer.valueOf(endDate.substring(3, 5)));
+        if (dStartDate.compareTo(dEndDate) > 0) {
+        	respWriter.print("Start date is later than end date.");
+        	throw new IllegalArgumentException();
+        }
         logger.log(Level.INFO,userAccount);
-//        List<Entity> transList = Transaction.getTransactionList(userAccount);
         List<Entity> transList = Transaction.getTransactionList(userAccount);
         int listLength = transList.size();
-        TreeSet<String> transCategories = new TreeSet<>();
-        TreeMap<String, Double> categoryAmountMap = new TreeMap<>();
+        Set<String> transCategories = new TreeSet<>();
+        Map<String, Double> categoryAmountMap = new TreeMap<>();
+        JSONObject returnData = new JSONObject();
+        JSONArray returnArr = new JSONArray();
         for (int i = 0; i < listLength; i++) {
+        	String thisDateProperty = (String) transList.get(i).getProperty("createDate");
+        	Date dThisDate = new Date(Integer.valueOf(thisDateProperty.substring(6)) - 1900,
+            		Integer.valueOf(thisDateProperty.substring(0, 2)) - 1,
+            			Integer.valueOf(thisDateProperty.substring(3, 5)));
+            //check
+            System.out.println(dThisDate);
+        	if (dThisDate.compareTo(dStartDate) < 0 || dThisDate.compareTo(dEndDate) > 0) {
+        		break;
+        	}
             String thisProperty = (String) transList.get(i).getProperty("category");
             transCategories.add(thisProperty);
-            categoryAmountMap.put(thisProperty, 0.0);
+        	if (categoryAmountMap.get(thisProperty) != null) {
+        		Double oldAmount = Double.valueOf(categoryAmountMap.get(thisProperty));
+        		Double thisAmount = Double.valueOf((String) transList.get(i).getProperty("amount"));
+        		categoryAmountMap.put(thisProperty, oldAmount + thisAmount);
+        	} else {
+        		categoryAmountMap.put(thisProperty, Double.valueOf((String) transList.get(i).getProperty("amount")));
+        	}
         }
+        /*
         for (int i = 0; i < listLength; i++) {
             String thisAmountString = (String) transList.get(i).getProperty("amount");
             String thisProperty = (String) transList.get(i).getProperty("category");
@@ -54,14 +87,13 @@ public class SpendingCategoryReportServlet extends HttpServlet {
             categoryAmountMap.put(thisProperty, newAmount);
         }
         Object[] categories = transCategories.toArray();
+        
 //        JSONObject returnData = new JSONObject();
 //        JSONObject returnObject = new JSONObject();
 //        returnData.putAll(categoryAmountMap);
 //        returnObject.put("Data", returnData);
 //        returnObject.writeJSONString(respWriter);
         //		JSONArray returnArray = new JSONArray();'
-        JSONObject returnData = new JSONObject();
-        JSONArray returnArr = new JSONArray();
        // JSONObject returnObj = new JSONObject();
         logger.log(Level.INFO,categoryAmountMap.size() + "");
         for (int i = 0; i < categoryAmountMap.size(); i++) {
@@ -71,6 +103,12 @@ public class SpendingCategoryReportServlet extends HttpServlet {
             thisObj.put(newCategory, newAmount);
             returnArr.add(thisObj);
 //			returnArray.add(returnString);
+        }
+        */
+        for (String s : transCategories) {
+        	JSONObject thisObj = new JSONObject();
+        	thisObj.put(s, String.valueOf(categoryAmountMap.get(s)));
+        	returnArr.add(thisObj);
         }
         returnData.put("Data", returnArr);
        // returnData.put("Data", returnObj);
